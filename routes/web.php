02d37f1b8802e6fc1867824637dbbeed650af383
shinya <in.go.b.shin@gmail.com>
2026-04-20 16:22:26 +0900

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,14 @@
 // Sanctum との CSRF Cookie 取得端点
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Laravel\Sanctum\Http\Controllers\CsrfCookieController;
 
 // sanctum/csrf-cookie エンドポイント
 // GET で呼ばれる（axios が GET で呼んでいるため）
-Route::get('/sanctum/csrf-cookie', function () {
-    return response()->json([
+Route::get('/sanctum/csrf-cookie', function (Request $request) {
+    $response = app(CsrfCookieController::class)->show($request);
+
+    return $response->setContent(json_encode([
         'csrf_token' => csrf_token(),
-    ]);
+    ]))->header('Content-Type', 'application/json');
 })->middleware('web');
